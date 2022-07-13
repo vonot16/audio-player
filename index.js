e16d76636e7c9861f3dbffff195ecff279552337
vonot16 <rafaelfeliperm@gmail.com>
2022-07-13 14:20:07 -0300

    seek bar working

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-window.onload = () =>{
+window.onload = () => {
     const audio = document.querySelector("audio")
+    const currentSeek = document.querySelector(".current-seek-bar")
     const playButton = document.querySelector(".play")
     const pauseButton = document.querySelector(".pause")
+    const durationLabel = document.querySelector(".duration")
+    const currentLabel = document.querySelector(".current")
+    const audioDuration = audio.duration
 
-    playButton.addEventListener("click", playPause)
-    pauseButton.addEventListener("click", playPause)
+    addEvents()
+    loadAudioInformation()
 
-    function playPause(){
+    setInterval(updateAudioStatus, 1000)
+
+    function loadAudioInformation(){
+        
+
+        durationLabel.innerText = formatTime(audioDuration)
+    }
+
+    function updateAudioStatus() {
+        if (!audio.paused) {
+            let audioCurrent = audio.currentTime
+            let percentage = `${audioCurrent / audioDuration * 100}%`
+            currentSeek.style.width = percentage
+
+        }
+    }
+
+    function formatTime(time){
+        let minutes = time/60
+        let seconds = time%60
+
+        return `${minutes.toFixed(0).padStart(2, '0')}:${seconds.toFixed(0).padStart(2, '0')}`
+    }
+
+    function addEvents() {
+        playButton.addEventListener("click", playPause)
+        pauseButton.addEventListener("click", playPause)
+    }
+
+    function playPause() {
         audio.paused ? audio.play() : audio.pause()
         playButton.classList.toggle("hidden")
         pauseButton.classList.toggle("hidden")
